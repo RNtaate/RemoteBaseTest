1fe1dc9fe99719f9d6ff54bfeebcc4f1cf698d95
RNtaate <royhyde14@gmail.com>
2022-04-14 23:29:59 +0300

    Display entered movies

--- src/components/Movieslist.js
+++ src/components/Movieslist.js
@@ -1,27 +1,31 @@
 import React from 'react'
 
-function Movieslist() {
+function Movieslist({ actualMovieList }) {
   return (
     <section>
       <ul 
         className='styled w-100 pl-0' 
         data-testid='moviesList'
       >
-      <li 
+      { actualMovieList.map((movie, index) => {
+
+      return (<li 
         className='flex slide-up-fade-in justify-content-between'
         style={{borderBottom: '2px solid var(--primary-color)'}}
+        key={index}
       >
         <div className='layout-column w-40'>
           {/* use this header for movie name */}
-          <h3 className='my-3'></h3>
+          <h3 className='my-3'>{movie.moviename}</h3>
           {/* use this paragraph for movie ratings, for example: 'Ratings: 88/100' */}
-          <p className='my-0'></p>
+          <p className='my-0'>{movie.rating} / 100</p>
         </div>
         <div className='layout-row my-auto mr-20'>
           {/* use this paragraph for movie duration, for example: '2.5 Hrs' */}
-          <p className='justify-content-end'></p>
+          <p className='justify-content-end'>{movie.duration}</p>
         </div>
-      </li>
+      </li>)
+      })}
       </ul>
     </section>
   )
