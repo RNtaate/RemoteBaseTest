de506487cabc10f0343b17b8819723a00737ddd0
RNtaate <royhyde14@gmail.com>
2022-04-14 23:30:42 +0300

    Provide local state as props

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ const title = 'Favorite Movie Directory'
 
 function App() {
   const [actualMovieList, setActualMovieList] = useState([]);
-  const [err, setErr] = useState('');
+  const [actualSearchList, setActualSearchList] = useState([]);
+  
   return (
     <div>
       <h8k-navbar header={ title } />
       <div className='layout-row justify-content-center mt-100'>
         <div className='w-30 mr-75'>
-          <Movieform />
+          <Movieform setActualMovieList={setActualMovieList} actualMovieList={actualMovieList}/>
         </div>
         <div className='layout-column w-30'>
           <Search />
-          <Movieslist /> 
-          <div data-testid='noResult'>
-            <h3 className='text-center'>No Results Found</h3>
-          </div>
+          {actualMovieList.length != 0 && 
+          <>
+            <Movieslist actualMovieList={actualMovieList}/> :
+            <div data-testid='noResult'>
+              <h3 className='text-center'>No Results Found</h3>
+            </div>
+          </>}
         </div>
       </div> 
     </div>
