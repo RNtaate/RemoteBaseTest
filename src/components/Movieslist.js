77461d612ae9ed9975479a79c6ac079cd4b7947b
RNtaate <royhyde14@gmail.com>
2022-11-15 13:15:15 +0300

    Update design by removing space in ratings

--- src/components/Movieslist.js
+++ src/components/Movieslist.js
@@ -23,7 +23,7 @@ function Movieslist({ actualMovieList, actualSearchList }) {
           {/* use this header for movie name */}
           <h3 className='my-3'>{movie.moviename}</h3>
           {/* use this paragraph for movie ratings, for example: 'Ratings: 88/100' */}
-          <p className='my-0'>{movie.rating} / 100</p>
+          <p className='my-0'>{movie.rating}/100</p>
         </div>
         <div className='layout-row my-auto mr-20'>
           {/* use this paragraph for movie duration, for example: '2.5 Hrs' */}
