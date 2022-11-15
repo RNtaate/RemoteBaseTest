e4d37afbd43ece4ef85e4ee681a56d4febac2a31
RNtaate <royhyde14@gmail.com>
2022-11-15 12:04:12 +0300

    Create updateSearchList function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
-function Search() {
+function Search({ setActualSearchList, actualMovieList, setSearchFocus }) {
+
+  const searchRef = useRef();
+
+  function updateSearchList() {
+    if (searchRef.current.value.length >= 2) {
+      setSearchFocus(true);
+      let newArray = actualMovieList.filter(movie => movie.moviename.toLowerCase().includes(searchRef.current.value.toString().toLowerCase()));
+      setActualSearchList(newArray);
+    }else {
+      setSearchFocus(false);
+      setActualSearchList([]);
+    }
+  }
 
   return (
     <section className='layout-row justify-content-center mb-40'>
-      <input 
+      <input
         type='text'
-        placeholder='Search for movie by name' 
+        placeholder='Search for movie by name'
         className='w-75 py-2'
         data-testid='search'
+        ref = {searchRef}
+        onInput={updateSearchList}
+        onBlur={() => {
+          setSearchFocus(false);
+        }}
       />
     </section>
   )
